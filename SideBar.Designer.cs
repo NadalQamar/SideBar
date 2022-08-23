4eab780f772508cb74218efc9021159c66d1e95f
Nadal Qamar <qamar_173na@outlook.com>
2022-08-23 19:39:26 +0500

    Add project files.

---
+++ SideBar.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace SideBar
+{
+    partial class SideBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ControlBox = new Label();
+            this.SuspendLayout();
+
+            //
+            // ControlBox
+            //
+
+            this.ControlBox.Size = new Size(10, Screen.PrimaryScreen.Bounds.Height);
+            this.ControlBox.BringToFront();
+            this.ControlBox.Location = new Point(0, 0);
+            this.ControlBox.MouseEnter += new System.EventHandler(this.Mouse_Enter);
+            
+            // 
+            // SideBar
+            //
+
+            this.ClientSize = new Size(40, Screen.PrimaryScreen.Bounds.Height);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = Location = new System.Drawing.Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SideBar";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "SideBar";
+            this.TopMost = true;
+            this.Controls.Add(this.ControlBox);
+            this.Load += new System.EventHandler(this.SideBar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label ControlBox;
+    }
+}
---
+++ SideBar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SideBar
+{
+    public partial class SideBar : Form
+    {
+        bool accessTag = false;
+        public SideBar()
+        {
+            InitializeComponent();
+        }
+
+        private void SideBar_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Mouse_Enter(object sender, EventArgs e)
+        {// Hide and Unhide
+            if(accessTag == false)
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 40, 0);
+                accessTag = true;
+            }
+            else
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
+                accessTag = false;
+            }
+        }
+
+
+    }
+}
