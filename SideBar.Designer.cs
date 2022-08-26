a96b13f5632e47dcc8fc6a0fa6b40a68340eab78
Nadal Qamar <qamar_173na@outlook.com>
2022-08-26 16:42:01 +0500

    Added Three Buttons with their Functions

--- SideBar.Designer.cs
+++ SideBar.Designer.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using System.Drawing;
+using System.ComponentModel;
 
 namespace SideBar
 {
@@ -31,41 +33,99 @@ namespace SideBar
         /// </summary>
         private void InitializeComponent()
         {
-            this.ControlBox = new Label();
+            this.components = new System.ComponentModel.Container();
+            this.ControlBox = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.visiblityButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.closeButtonInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.visiblityButtonInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.settingsButtonInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.SideBarPrimaryButtonPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SideBarPrimaryButtonPanel.SuspendLayout();
             this.SuspendLayout();
-
-            //
+            // 
             // ControlBox
-            //
-
-            this.ControlBox.Size = new Size(10, Screen.PrimaryScreen.Bounds.Height);
-            this.ControlBox.BringToFront();
-            this.ControlBox.Location = new Point(0, 0);
-            this.ControlBox.MouseEnter += new System.EventHandler(this.Mouse_Enter);
-            
+            // 
+            this.ControlBox.BackColor = System.Drawing.Color.Transparent;
+            this.ControlBox.Location = new System.Drawing.Point(0, 0);
+            this.ControlBox.Name = "ControlBox";
+            this.ControlBox.Size = new System.Drawing.Size(10, Screen.PrimaryScreen.Bounds.Height);
+            this.ControlBox.TabIndex = 2;
+            this.ControlBox.MouseEnter += new System.EventHandler(this.Mouse_Enter);    
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Location = new System.Drawing.Point(0, 0);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(35, 30);
+            this.settingsButton.TabIndex = 1;
+            this.settingsButton.Text = "Ѕ";
+            this.settingsButtonInfo.SetToolTip(this.settingsButton, "Settings");
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // visiblityButton
+            // 
+            this.visiblityButton.Location = new System.Drawing.Point(0, 40);
+            this.visiblityButton.Name = "visiblityButton";
+            this.visiblityButton.Size = new System.Drawing.Size(35, 30);
+            this.visiblityButton.TabIndex = 2;
+            this.visiblityButton.Text = "V";
+            this.visiblityButtonInfo.SetToolTip(this.visiblityButton, "Visiblity");
+            this.visiblityButton.UseVisualStyleBackColor = true;
+            this.visiblityButton.Click += new System.EventHandler(this.visiblityButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(0, 80);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(35, 30);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Χ";
+            this.closeButtonInfo.SetToolTip(this.closeButton, "Close");
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // SideBarPrimaryButtonPanel
+            // 
+            this.SideBarPrimaryButtonPanel.Controls.Add(this.settingsButton);
+            this.SideBarPrimaryButtonPanel.Controls.Add(this.visiblityButton);
+            this.SideBarPrimaryButtonPanel.Controls.Add(this.closeButton);        
+            this.SideBarPrimaryButtonPanel.Location = new System.Drawing.Point(0, Screen.PrimaryScreen.Bounds.Height-110);
+            this.SideBarPrimaryButtonPanel.Name = "SideBarPrimaryButtonPanel";
+            this.SideBarPrimaryButtonPanel.Size = new System.Drawing.Size(40, 110);
             // 
             // SideBar
-            //
-
-            this.ClientSize = new Size(40, Screen.PrimaryScreen.Bounds.Height);
+            // 
+            this.ClientSize = new System.Drawing.Size(40, Screen.PrimaryScreen.Bounds.Height);
+            this.Controls.Add(this.SideBarPrimaryButtonPanel);
+            this.Controls.Add(this.ControlBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.StartPosition = FormStartPosition.Manual;
-            this.Location = Location = new System.Drawing.Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SideBar";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Location = Location = new System.Drawing.Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
             this.Text = "SideBar";
             this.TopMost = true;
-            this.Controls.Add(this.ControlBox);
             this.Load += new System.EventHandler(this.SideBar_Load);
+            this.SideBarPrimaryButtonPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Label ControlBox;
+        private new Label ControlBox;
+        private Button closeButton;
+        private Button visiblityButton;
+        private Button settingsButton;
+        private ToolTip closeButtonInfo;
+        private ToolTip visiblityButtonInfo;
+        private ToolTip settingsButtonInfo;
+        private FlowLayoutPanel SideBarPrimaryButtonPanel;
     }
 }
--- SideBar.cs
+++ SideBar.cs
@@ -12,31 +12,82 @@ namespace SideBar
 {
     public partial class SideBar : Form
     {
-        bool accessTag = false;
+        bool accessFlag = false;
+        bool settingFlag = false;
+
         public SideBar()
         {
             InitializeComponent();
         }
-
         private void SideBar_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void Mouse_Enter(object sender, EventArgs e)
         {// Hide and Unhide
-            if(accessTag == false)
+            if(accessFlag == true && settingFlag == true)
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
+                ClientSize = new Size(40, Screen.PrimaryScreen.Bounds.Height);
+                SideBarPrimaryButtonPanel.Location = new System.Drawing.Point(0, Screen.PrimaryScreen.Bounds.Height - 110);
+                accessFlag = false;
+                settingFlag = false;
+            }
+            else if(accessFlag == false)
             {
                 Location = new Point(Screen.PrimaryScreen.Bounds.Width - 40, 0);
-                accessTag = true;
+                accessFlag = true;
             }
             else
             {
                 Location = new Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
-                accessTag = false;
+                accessFlag = false;
             }
         }
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            if (settingFlag == false)
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 550, 0);
+                ClientSize = new Size(550, Screen.PrimaryScreen.Bounds.Height);
+                SideBarPrimaryButtonPanel.Location = new System.Drawing.Point(ClientSize.Width - 40, Screen.PrimaryScreen.Bounds.Height - 110);
+                settingFlag = true;
+            }
+            else
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 40, 0);
+                ClientSize = new Size(40, Screen.PrimaryScreen.Bounds.Height);
+                SideBarPrimaryButtonPanel.Location = new System.Drawing.Point(0, Screen.PrimaryScreen.Bounds.Height - 110);
+                settingFlag = false;
+            }
+        }
+        private void visiblityButton_Click(object sender, EventArgs e)
+        {
+            if(accessFlag == true && settingFlag == true && ControlBox.Enabled == true)
+            {
+                ControlBox.Enabled = false;
+            }
+            else if (accessFlag == true && settingFlag == true && ControlBox.Enabled == false)
+            {
+                ControlBox.Enabled = true;
+            }
+            else if(ControlBox.Enabled == true)
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 40, 0);
+                ControlBox.Enabled = false;
+            }else
+            {
+                Location = new Point(Screen.PrimaryScreen.Bounds.Width - 1, 0);
+                ControlBox.Enabled = true;
+            }
 
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
 
     }
 }
